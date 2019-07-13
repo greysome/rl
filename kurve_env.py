58257d089495d6894b9f8e543cfe0e8e0eaf53d4
greysome <wayyan.win@gmail.com>
2019-07-13 18:19:14 +0800

    fix kurve_env render_screen option

--- kurve_env.py
+++ kurve_env.py
@@ -6,25 +6,27 @@ from kurve_engine import KurveEngine
 # suppress welcome message
 with contextlib.redirect_stdout(None):
     import pygame
-    from pygame.locals import *
+    from pygame.locals import DOUBLEBUF
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 class KurveSinglePlayer(gym.core.Env):
-    def __init__(self):
+    def __init__(self, render_screen=True):
+        self.render_screen = render_screen
         self.action_space = spaces.Discrete(2)
         self.observation_space = spaces.Box(low=0, high=708, shape=(30,))
         self.engine = KurveEngine(n_players=1, w=500, h=500,
                                   fov=150, sector_theta=5)
-        if self.render:
+
+        if self.render_screen:
             self._pygame_init()
 
         self.update_circle = None
     
     def reset(self):
         self.engine.reset()
-        if self.render:
+        if self.render_screen:
             self.body_sfc.fill(BLACK)
             self.screen.blit(self.body_sfc, (0, 0))
             pygame.display.flip()
@@ -34,7 +36,7 @@ class KurveSinglePlayer(gym.core.Env):
         self.engine.step([(0, a)])
         done, _ = self.engine.game_ended()
         r = (0 if done else 1)
-        if self.render:
+        if self.render_screen:
             self._render_player()
         return self.engine.observe(0), r, done, {}
 
@@ -49,8 +51,8 @@ class KurveSinglePlayer(gym.core.Env):
         pygame.init()
         pygame.event.set_allowed([])
         pygame.display.set_caption('kurve')
-        self.screen = pygame.display.set_mode((self.engine.w,
-                                               self.engine.h), DOUBLEBUF)
+        self.screen = pygame.display.set_mode((self.engine.w, self.engine.h),
+                                              DOUBLEBUF)
         self.screen.set_alpha(None)
         self.body_sfc = pygame.Surface((self.engine.w, self.engine.h))
 
@@ -58,6 +60,6 @@ class KurveSinglePlayer(gym.core.Env):
         p = self.engine._find_player(0)
         if not p.is_drawing_gap():
             self.update_circle = pygame.draw.circle(self.body_sfc,
-                                                     WHITE,
-                                                     (round(p.x),
-                                                      round(p.y)), p.r)
+                                                    WHITE,
+                                                    (round(p.x), round(p.y)),
+                                                    p.r)
--- main.py
+++ main.py
@@ -3,16 +3,13 @@ import gym
 import tensorflow as tf
 
 from learners import *
-from estimators import *
-from experience import *
-from explorers import *
 from sessions import *
 
 from kurve_env import KurveSinglePlayer
 
 if __name__ == '__main__':
-    agent = QLearning(env=KurveSinglePlayer, checkpoint='good-kurve/model-160',
-                      render=False,
-                      estimator_params={'layer_spec': (100,100), 'lr':
-                                        0.01, 'activation': tf.nn.relu})
+    agent = QLearning(env=KurveSinglePlayer, checkpoint='kurve-checkpoints/model-160',
+                      render=True, env_params={'render_screen': True},
+                      estimator_params={'layer_spec': (100,100), 'lr': 0.01,
+                                        'activation': tf.nn.relu})
     run_session(agent, TrainSession, plot=True)
